3d1f4f611e53035fb5dbe27ab14a9f1c8ced04f7
Zhi Li <zhili128@outlook.com>
2018-07-13 16:27:56 -0700

    VS: replace IServiceProvider with IAsyncServiceProvider

--- src/NuGet.Clients/NuGet.VisualStudio.Implementation/Extensibility/VsPathContextProvider.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.Implementation/Extensibility/VsPathContextProvider.cs
@@ -19,7 +19,6 @@ using NuGet.PackageManagement.VisualStudio;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
 using NuGet.ProjectManagement;
-using NuGet.ProjectManagement.Projects;
 using NuGet.ProjectModel;
 using NuGet.VisualStudio.Implementation.Resources;
 
@@ -32,31 +31,53 @@ namespace NuGet.VisualStudio
     public sealed class VsPathContextProvider : IVsPathContextProvider2
     {
         private const string ProjectAssetsFile = "ProjectAssetsFile";
-        private readonly IServiceProvider _serviceprovider;
         private readonly Lazy<ISettings> _settings;
         private readonly Lazy<IVsSolutionManager> _solutionManager;
         private readonly Lazy<NuGet.Common.ILogger> _logger;
         private readonly Lazy<IVsProjectAdapterProvider> _vsProjectAdapterProvider;
         private readonly Func<string, LockFile> _getLockFileOrNull;
-
+        private readonly Microsoft.VisualStudio.Threading.AsyncLazy<EnvDTE.DTE> _dte;
         private readonly Lazy<INuGetProjectContext> _projectContext = new Lazy<INuGetProjectContext>(() => new VSAPIProjectContext());
+        
 
         [ImportingConstructor]
         public VsPathContextProvider(
-            [Import(typeof(SVsServiceProvider))]
-            IServiceProvider serviceProvider,
             Lazy<ISettings> settings,
             Lazy<IVsSolutionManager> solutionManager,
             [Import("VisualStudioActivityLogger")]
             Lazy<NuGet.Common.ILogger> logger,
             Lazy<IVsProjectAdapterProvider> vsProjectAdapterProvider)
+            : this(AsyncServiceProvider.GlobalProvider,
+                  settings,
+                  solutionManager,
+                  logger,
+                  vsProjectAdapterProvider)
+        { }
+
+        public VsPathContextProvider(
+            IAsyncServiceProvider asyncServiceProvider,
+            Lazy<ISettings> settings,
+            Lazy<IVsSolutionManager> solutionManager,
+            Lazy<NuGet.Common.ILogger> logger,
+            Lazy<IVsProjectAdapterProvider> vsProjectAdapterProvider)
         {
-            _serviceprovider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            if (asyncServiceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(asyncServiceProvider));
+            }
+
             _settings = settings ?? throw new ArgumentNullException(nameof(settings));
             _solutionManager = solutionManager ?? throw new ArgumentNullException(nameof(solutionManager));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _vsProjectAdapterProvider = vsProjectAdapterProvider ?? throw new ArgumentNullException(nameof(vsProjectAdapterProvider));
             _getLockFileOrNull = BuildIntegratedProjectUtility.GetLockFileOrNull;
+
+            _dte = new Microsoft.VisualStudio.Threading.AsyncLazy<EnvDTE.DTE>(
+                async () =>
+                {
+                    return await asyncServiceProvider.GetDTEAsync();
+                },
+                NuGetUIThreadHelper.JoinableTaskFactory);
         }
 
         /// <summary>
@@ -145,8 +166,7 @@ namespace NuGet.VisualStudio
         {
             await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-            var dte = _serviceprovider.GetDTE();
-            var supportedProjects = dte.Solution.Projects.Cast<EnvDTE.Project>();
+            var supportedProjects = (await _dte.GetValueAsync()).Solution.Projects.Cast<EnvDTE.Project>();
 
             foreach (var solutionProject in supportedProjects)
             {
